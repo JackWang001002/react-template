dc749f507f7cfc93f3e85aafde313888df00ea30
jinbo.wang <che3vinci@gmail.com>
2022-06-13 19:01:13 +0800

    refactor: remove charaui

--- templates/react-vite/index.tsx
+++ templates/react-vite/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
       <App />
-    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('app')
 );
